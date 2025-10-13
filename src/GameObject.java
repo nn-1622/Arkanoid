3ea5475d0e4fee24059baa70b2f008ad44ec3e6a
lennoxiepeal <dinhvantien572006@gmail.com>
2025-10-13 07:51:12 +0700

    Initial commit

---
+++ src/GameObject.java
@@ -0,0 +1,12 @@
+public abstract class GameObject {
+    protected double x;
+    protected double y;
+    public GameObject(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public GameObject() {
+
+    }
+}
---
+++ src/MovableObject.java
@@ -0,0 +1,25 @@
+public class MovableObject extends GameObject {
+    private double x;
+    private double y;
+    public MovableObject(double x, double y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void move(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
+}
